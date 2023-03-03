e6b97992c3dc44cf01ab141019062d477681235c
devjean <jeancarloleall@gmail.com>
2023-03-03 13:28:57 -0300

    forntend

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,6 +2,8 @@ const { Router } = require("express");
 const routes = Router();
 
 const clientController = require("./modules/Cliente/ClienteControler");
+const livroController = require("./modules/Livro/LivroControler");
+const aluguelController = require("./modules/Aluguel/AluguelControler");
 
 routes.get("/", (req, res) => {
   return res.json("Server Up");
@@ -9,5 +11,13 @@ routes.get("/", (req, res) => {
 
 routes.get("/cliente", clientController.get);
 routes.post("/cliente", clientController.create);
+
+routes.get("/livro", livroController.get);
+routes.post("/livro", livroController.create);
+routes.post("/livro/:id", livroController.putIsAlterado);
+
+routes.get("/aluguel", aluguelController.get);
+routes.post("/aluguel", aluguelController.create);
+routes.put("/aluguel", aluguelController.put);
 
 module.exports = { routes };
