6a0c27b31dcb01cb39f19df9ed0df9b6df039e80
devjean <jeancarloleall@gmail.com>
2023-03-03 11:10:59 -0300

    backend

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,8 +1,11 @@
 const { Router } = require("express");
 const routes = Router();
+const clientController = require("./modules/Cliente/ClienteControler");
 
 routes.get("/", (req, res) => {
   return res.json("Server Up");
 });
+routes.get("/cliente", clientController.get);
+routes.post("/cliente", clientController.create);
 
 module.exports = { routes };
