070e52086cea9ae95f36f6de09febb35249a548c
devjean <jeancarloleall@gmail.com>
2023-03-03 11:32:57 -0300

    ignore

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,12 @@
 const { Router } = require("express");
 const routes = Router();
+
 const clientController = require("./modules/Cliente/ClienteControler");
 
 routes.get("/", (req, res) => {
   return res.json("Server Up");
 });
+
 routes.get("/cliente", clientController.get);
 routes.post("/cliente", clientController.create);
 
