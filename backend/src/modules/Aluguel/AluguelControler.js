026aee41d6859024bcce596af61739f7e0506925
devjean <jeancarloleall@gmail.com>
2023-03-03 13:55:55 -0300

    api

--- backend/src/modules/Aluguel/AluguelControler.js
+++ backend/src/modules/Aluguel/AluguelControler.js
@@ -1,18 +1,15 @@
 const moment = require("moment");
-const { PrismaCliente } = require("@prisma/client");
-const prisma = new PrismaCliente();
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
 async function create(req, res) {
-  console.log("Criando um novo aluguel");
-  console.log("Corpo da solicitação", req.body);
   const aluguel = await prisma.aluguel.create({
     data: req.body,
   });
   res.json(aluguel);
 }
 
-async function get() {
-  console.log("Obtendo todos os aluguéis...");
+async function get(req, res) {
   res.json(
     await prisma.aluguel.findMany({
       include: {
@@ -24,19 +21,22 @@ async function get() {
 }
 
 async function put(req, res) {
-  console.log("Atualizando aluguel...");
   let { data, dataDevolucao } = req.body;
+
   let dias = moment(dataDevolucao).diff(data, "days");
+
   req.body.valorArrecadado =
     parseFloat(dias) * parseFloat(req.body.valorDiaria);
 
   delete req.body.valorDiaria;
+
   const aluguel = await prisma.aluguel.update({
     data: req.body,
     where: {
       id: req.body.id,
     },
   });
+
   return res.json(aluguel);
 }
 
